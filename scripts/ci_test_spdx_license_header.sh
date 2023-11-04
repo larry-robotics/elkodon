0ea204a7d37e0c92648225558b8cfccdf1e74e85
Christian Eltzschig <me@elchris.org>
2023-11-04 19:37:15 +0100

    [#3] Finalize spdx license check script

--- scripts/ci_test_spdx_license_header.sh
+++ scripts/ci_test_spdx_license_header.sh
@@ -3,25 +3,50 @@
 cd $(git rev-parse --show-toplevel)
 
 RET_VAL=0
-FILES=$(find . -iwholename "*.rs")
-CHECK_LICENSE="// SPDX-License-Identifier: Apache-2.0"
 
-for FILE in $FILES
-do
-    FIRST_LINE=$(head -n 1 $FILE)
-    SECOND_LINE=$(head -n 2 $FILE | tail -n 1)
+check_license_header() {
+    FILES=$(find . -type f -iwholename "${FILE_SUFFIX}" )
+    CHECK_LICENSE="${COMMENT_SYMBOL} SPDX-License-Identifier: Apache-2.0"
 
-    if [[ "$FIRST_LINE" != "$CHECK_LICENSE" ]]
-    then
-        echo "$FILE :: missing license header \"$CHECK_LICENSE\""
-        RET_VAL=1
-    fi
+    for FILE in $FILES
+    do
+        FIRST_LINE=$(head -n 1 $FILE)
+        SECOND_LINE=$(head -n 2 $FILE | tail -n 1)
 
-    if [[ "$SECOND_LINE" != "" ]]
-    then
-        echo "$FILE :: missing new line after license header"
-        RET_VAL=1
-    fi
-done
+        if [[ "$FIRST_LINE" != "$CHECK_LICENSE" ]]
+        then
+            echo "$FILE :: missing license header \"$CHECK_LICENSE\""
+            RET_VAL=1
+        fi
+
+        if [[ "$SECOND_LINE" != "" ]]
+        then
+            echo "$FILE :: missing new line after license header"
+            RET_VAL=1
+        fi
+    done
+}
+
+check_rust() {
+    FILE_SUFFIX="*.rs"
+    COMMENT_SYMBOL="//"
+    check_license_header
+}
+
+check_shell() {
+    FILE_SUFFIX="*.sh"
+    COMMENT_SYMBOL="#"
+    check_license_header
+}
+
+check_toml() {
+    FILE_SUFFIX="*.toml"
+    COMMENT_SYMBOL="#"
+    check_license_header
+}
+
+check_rust
+check_shell
+check_toml
 
 exit $RET_VAL
