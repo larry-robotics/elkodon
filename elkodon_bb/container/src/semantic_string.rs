b684bbbf0496daa3eb556b65b52e7f08eef2d67d
Mathias Kraus <elboberido@m-hias.de>
2023-12-09 14:53:24 +0100

    [#3] Use 'enum_gen' macro

--- elkodon_bb/container/src/semantic_string.rs
+++ elkodon_bb/container/src/semantic_string.rs
@@ -1,17 +1,19 @@
 use crate::byte_string::{
     as_escaped_string, strlen, FixedSizeByteString, FixedSizeByteStringModificationError,
 };
+use elkodon_bb_elementary::enum_gen;
 use elkodon_bb_log::fail;
 use std::fmt::{Debug, Display};
 use std::hash::Hash;
 use std::ops::Deref;
 
-/// Errors that can occure while modifying a [`SemanticString`].
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum SemanticStringError {
+enum_gen! {SemanticStringError
+  entry:
     InvalidCharacter,
-    InvalidName,
-    ExceedsMaximumLength,
+    InvalidName
+
+  generalization:
+    ExceedsMaximumLength <= FixedSizeByteStringModificationError
 }
 
 impl std::fmt::Display for SemanticStringError {
@@ -21,16 +23,6 @@ impl std::fmt::Display for SemanticStringError {
 }
 
 impl std::error::Error for SemanticStringError {}
-
-impl From<FixedSizeByteStringModificationError> for SemanticStringError {
-    fn from(value: FixedSizeByteStringModificationError) -> Self {
-        match value {
-            FixedSizeByteStringModificationError::InsertWouldExceedCapacity => {
-                SemanticStringError::ExceedsMaximumLength
-            }
-        }
-    }
-}
 
 pub trait SemanticStringAccessor<const CAPACITY: usize> {
     /// Creates a new empty SemanticStringAccessor which may violates the content contract.
