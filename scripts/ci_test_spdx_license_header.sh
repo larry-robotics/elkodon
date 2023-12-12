9591064f37d3865e5809e15908bf7d59ff81563a
Christian Eltzschig <me@elchris.org>
2023-12-12 22:08:51 +0100

    [#3] Add license header check

--- scripts/ci_test_spdx_license_header.sh
+++ scripts/ci_test_spdx_license_header.sh
@@ -14,27 +14,19 @@
 
 cd $(git rev-parse --show-toplevel)
 
-REQUIRED_LICENSE_HEADER="SPDX-License-Identifier: Apache-2.0"
 RET_VAL=0
 
 check_license_header() {
-    FILES=$(find . -type f -iwholename "${FILE_SUFFIX}" )
-    CHECK_LICENSE="${COMMENT_SYMBOL} ${REQUIRED_LICENSE_HEADER}"
+    FILES=$(find . -type f -iwholename "${FILE_SUFFIX}" -not -path "./target/*" )
 
     for FILE in $FILES
     do
-        FIRST_LINE=$(head -n 1 $FILE)
-        SECOND_LINE=$(head -n 2 $FILE | tail -n 1)
+        HAS_CORRECT_HEADER=$(diff <(head -n 12 $FILE | tail -n 11 | sed "s/$COMMENT_SYMBOL\(.*\)/\1/") <(head -n 12 scripts/copyright_header.txt | tail -n 11) | wc -l)
+        HAS_CORRECT_HEADER_YEAR_LINE=$(head -n 1 $FILE | grep -E "^$COMMENT_SYMBOL_GREP Copyright \(c\) 20[2-9][0-9] Contributors to the Eclipse Foundation\$" | wc -l)
 
-        if [[ "$FIRST_LINE" != "$CHECK_LICENSE" ]]
+        if [[ "$HAS_CORRECT_HEADER" != "0" ]] || [[ "$HAS_CORRECT_HEADER_YEAR_LINE" != "1" ]]
         then
-            echo "$FILE :: missing license header \"$CHECK_LICENSE\""
-            RET_VAL=1
-        fi
-
-        if [[ "$SECOND_LINE" != "" ]]
-        then
-            echo "$FILE :: missing new line after license header"
+            echo "The $FILE has a wrong license header."
             RET_VAL=1
         fi
     done
@@ -42,19 +34,22 @@ check_license_header() {
 
 check_rust() {
     FILE_SUFFIX="*.rs"
-    COMMENT_SYMBOL="//"
+    COMMENT_SYMBOL="\/\/"
+    COMMENT_SYMBOL_GREP="//"
     check_license_header
 }
 
 check_shell() {
     FILE_SUFFIX="*.sh"
     COMMENT_SYMBOL="#"
+    COMMENT_SYMBOL_GREP="#"
     check_license_header
 }
 
 check_toml() {
     FILE_SUFFIX="*.toml"
     COMMENT_SYMBOL="#"
+    COMMENT_SYMBOL_GREP="#"
     check_license_header
 }
 
